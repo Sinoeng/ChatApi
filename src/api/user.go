5b480507e5a656f8d3a39d9088d2e9378d8f57db
Simon Engström <simonspost2012@gmail.com>
2024-12-08 14:19:38 +0100

    api create user and login wip

--- src/api/user.go
+++ src/api/user.go
@@ -1,15 +1,39 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func new(c *gin.Context) {
-	println("Request body: ")
-	println(c.Request.Body)
+type User struct {
+	Name     string `form:"name" json:"name" xml:"name"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
 
-	println("query: ")
-	println(c.Query("name"))
+// TODO update status codes and look over error handling for ALL functions
+func new(c *gin.Context) { //TODO: add email
+	var usr User
+
+	if err := c.Bind(&usr); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	bytes, err := bcrypt.GenerateFromPassword([]byte(usr.Password), 11)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "could not hash password"})
+		return
+	}
+
+	if err := db.InsertNewUser(usr.Name, string(bytes)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "user created successfully"})
 }
 
 func verify(c *gin.Context) { // verify jwt
@@ -17,10 +41,47 @@ func verify(c *gin.Context) { // verify jwt
 }
 
 func login(c *gin.Context) { // issue jwt
+	var creds User
 
+	if err := c.Bind(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := db.GetUserByUsername(creds.Name)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "Logged in successfully"}) //TODO: issue jwt
 }
 
-func remove(c *gin.Context) {
+func remove(c *gin.Context) { // TODO: add authentication
+	var reqUsr User
+
+	if err := c.Bind(&reqUsr); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	dbUsr, err := db.GetUserByUsername(reqUsr.Name)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := db.DeleteUserByID(dbUsr.ID); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
 }
 
@@ -28,6 +89,8 @@ func AddUserRoutes(grp *gin.RouterGroup) {
 
 	user := grp.Group("/user")
 
-	user.GET("/new", new)
+	user.POST("/new", new)
+	user.POST("/login", login)
+	user.DELETE("/:user", remove)
 
 }
